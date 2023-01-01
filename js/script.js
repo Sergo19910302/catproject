88fdc463b369103cfc63ba429dadffca0c0a587f
sergeifeigin <sergeifeigin1991@gmail.com>
2023-01-01 22:23:44 +0200

    added few features and button video

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+$(document).ready(function() {
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+});
+    
